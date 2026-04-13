33e083205c77634d668b9e34f5d6cb7378e7cff1
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-04-13 19:53:14 +0530

    update Database.php

--- src/Database.php
+++ src/Database.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace App;
 
-class Database extends SQLite3
+use PDO;
+use PDOException;
+
+class Database extends PDO
 {
-    public function __construct()
+    public function __construct(string $file)
     {
-        $this->query;
+        $dns = 'sqlite:'.$file;
+
+        try {
+
+            parent::__construct($dns);
+
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            exit('Connection failed: '.$e->getMessage());
+        }
     }
 }
